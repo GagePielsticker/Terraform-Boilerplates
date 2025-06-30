f4c7fc9b199b9755a028e4b0a47af0b41eb07630
Gage Pielsticker <gagepays@gmail.com>
2025-06-30 00:31:55 -0500

    fmt

--- project_codepipeline/environments/dev/variable.tf
+++ project_codepipeline/environments/dev/variable.tf
@@ -1,32 +1,32 @@
 variable "env" {
-  default = "dev"
-  type = string
+  default     = "dev"
+  type        = string
   description = "Environment that is currently operating."
 }
 
 variable "github_connection_arn" {
-  default = "xxxxxxxx"
-  type = string
+  default     = "xxxxxxxx"
+  type        = string
   description = "The github app connection to authenticate codepipeline source with."
 }
 
 variable "repositories" { #Iterated through and create new pipeline for each
   type = list(object({
-    id = string
-    name = string
+    id     = string
+    name   = string
     branch = string
   }))
 
   default = [
     {
-      id = "org/repository" #org/repo
-      name = "my-proj" #Used in config names, shorthand, no spaces use - instead
-      branch = "dev" #branch to pull from for build
+      id     = "org/repository" #org/repo
+      name   = "my-proj"        #Used in config names, shorthand, no spaces use - instead
+      branch = "dev"            #branch to pull from for build
     },
-        {
-      id = "org/repository"
-      name = "my-proj"
+    {
+      id     = "org/repository"
+      name   = "my-proj"
       branch = "dev"
     },
-    ]
+  ]
 }
