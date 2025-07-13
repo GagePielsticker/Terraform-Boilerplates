9df7f5dbc1f4c44a9a771a66344edc22269df604
James Pielsticker <gagepays@gmail.com>
2025-07-12 19:50:00 -0500

    Cost Budget Alarms

---
+++ cost_alarms/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Project = "cost-budgets"
+    }
+  }
+}
+
+variable "limit_amount_USD" {
+  type    = string
+  default = "20"
+}
+variable "emails" {
+  type    = set(string)
+  default = ["test@test.com", "test2@test.com"]
+}
+
+resource "aws_budgets_budget" "account_budget" {
+  name         = "Account Budget"
+  budget_type  = "COST"
+  limit_amount = var.limit_amount_USD
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.emails
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = var.emails
+  }
+}
---
+++ cost_alarms/readme.md
@@ -0,0 +1,2 @@
+# Cost Alarm Module
+Set's up account budget using AWS budgets as well as email alerts for breaking 80% and 100% threshold for forecasted cost. Can be modularized fairly easy.
